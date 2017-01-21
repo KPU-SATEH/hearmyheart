e312de65d9fd89658cf3c944309b11e9bae4cb63
ByeonHarin <qusgkfls93@naver.com>
2017-01-21 23:26:45 +0900

    add function1

---
+++ Function1/app/src/main/java/com/example/function1/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.function1;
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+    boolean flag = false;
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+        surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(surfaceListener);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(flag) {
+                    surfaceView.setVisibility(View.INVISIBLE);
+                    flag = false;
+                }
+                else {
+                    surfaceView.setVisibility(View.VISIBLE);
+                    flag = true;
+                }
+            }
+        });
+
+    }
+
+    private SurfaceHolder.Callback surfaceListener = new SurfaceHolder.Callback(){
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            camera = Camera.open(1);
+            try{
+                camera.setPreviewDisplay(holder);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewSize(width, height);
+            camera.startPreview();
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            camera.release();
+            camera = null;
+        }
+    };
+
+}
